553254e71f9384a5e0d2cb04ea70431856eed1c3
Phillip Chu <misterpchu@gmail.com>
2021-08-06 20:06:49 -0700

    Made changes to database, models, main routes, and post template

--- app/main/routes.py
+++ app/main/routes.py
@@ -174,3 +174,16 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been deleted!", "success")
     return redirect(url_for("main.user", username=current_user.username))
+
+
+@main.route("/like/<int:post_id>/<action>")
+@login_required
+def like_action(post_id, action):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    if action == "like":
+        current_user.like_post(post)
+        db.session.commit()
+    if action == "unlike":
+        current_user.unlike_post(post)
+        db.session.commit()
+    return redirect(request.referrer)
--- app/models.py
+++ app/models.py
@@ -75,6 +75,24 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    liked = db.relationship(
+        "PostLike",
+        foreign_keys="PostLike.user_id",
+        backref="user", lazy="dynamic"
+    )
+
+    def like_post(self, post):
+        if not self.has_liked_post(post):
+            like = PostLike(user_id=self.id, post_id=post.id)
+            db.session.add(like)
+
+    def unlike_post(self, post):
+        if self.has_liked_post(post):
+            PostLike.query.filter_by(user_id=self.id, post_id=post.id).delete()
+
+    def has_liked_post(self, post):
+        return PostLike.query.filter(PostLike.user_id == self.id, PostLike.post_id == post.id).count() > 0
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -82,8 +100,16 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
+    likes = db.relationship("PostLike", backref="post", lazy="dynamic")
+
     def __repr__(self):
         return "<Post {}>".format(self.body)
+
+
+class PostLike(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
 
 
 @login.user_loader
