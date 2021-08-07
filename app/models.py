0ca06c1fffeb94be2d4217684933ca9c27bcea24
Phillip Chu <misterpchu@gmail.com>
2021-08-07 16:55:55 -0700

    Made changes to post content length

--- app/models.py
+++ app/models.py
@@ -96,7 +96,7 @@ class User(UserMixin, db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    body = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
