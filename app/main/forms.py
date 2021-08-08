2babbccc2f362849715c1c33c9a3baf136f9d451
Phillip Chu <misterpchu@gmail.com>
2021-08-07 17:17:19 -0700

    Made changes to forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 from app.models import User
