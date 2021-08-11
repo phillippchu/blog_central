81c161b7265b90d44970a368a4a3d1e708e71d32
Phillip Chu <misterpchu@gmail.com>
2021-08-10 19:43:38 -0700

    Redesigned website layout

--- app/main/routes.py
+++ app/main/routes.py
@@ -19,38 +19,34 @@ def blog_central():
     return render_template("blog_central.html")
 
 
-@main.route("/", methods=["GET", "POST"])
-@main.route("/index", methods=["GET", "POST"])
+# @main.route("/", methods=["GET", "POST"])
+# @main.route("/index", methods=["GET", "POST"])
+# @login_required
+# def index():
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         post = Post(body=form.post.data, author=current_user)
+#         db.session.add(post)
+#         db.session.commit()
+#         flash("Your post is now live!", "success")
+#         return redirect(url_for("main.index"))
+#     page = request.args.get("page", 1, type=int)
+#     posts = current_user.followed_posts().paginate(
+#         page, current_app.config["POSTS_PER_PAGE"], False)
+#     next_url = url_for(
+#         "main.index", page=posts.next_num) if posts.has_next else None
+#     prev_url = url_for(
+#         "main.index", page=posts.prev_num) if posts.has_prev else None
+#     return render_template("index.html", form=form, posts=posts.items, next_url=next_url, prev_url=prev_url, user=current_user)
+
+@main.route("/")
+@main.route("/index")
 @login_required
 def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash("Your post is now live!", "success")
-        return redirect(url_for("main.index"))
     page = request.args.get("page", 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, current_app.config["POSTS_PER_PAGE"], False)
-    next_url = url_for(
-        "main.index", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for(
-        "main.index", page=posts.prev_num) if posts.has_prev else None
-    return render_template("index.html", form=form, posts=posts.items, next_url=next_url, prev_url=prev_url, user=current_user)
-
-
-@main.route("/explore")
-@login_required
-def explore():
-    page = request.args.get("page", 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, current_app.config["POSTS_PER_PAGE"], False)
-    next_url = url_for(
-        "main.explore", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for(
-        "main.explore", page=posts.prev_num) if posts.has_prev else None
-    return render_template("explore.html", posts=posts.items, next_url=next_url, prev_url=prev_url)
+    posts = Post.query.order_by(
+        Post.timestamp.desc()).paginate(page=page, per_page=3)
+    return render_template("index.html", posts=posts, user=current_user)
 
 
 @main.route("/user/<username>", methods=["GET", "POST"])
@@ -58,14 +54,10 @@ def explore():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get("page", 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, current_app.config["POSTS_PER_PAGE"], False)
-    next_url = url_for("main.user", username=user.username,
-                       page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("main.user", username=user.username,
-                       page=posts.prev_num) if posts.has_prev else None
+    posts = user.posts.order_by(
+        Post.timestamp.desc()).paginate(page=page, per_page=3)
     form = EmptyForm()
-    return render_template("user.html", user=user, posts=posts.items, next_url=next_url, prev_url=prev_url, form=form)
+    return render_template("user.html", user=user, posts=posts, form=form)
 
 
 @main.route("/user/<string:username>/edit_profile", methods=["GET", "POST"])
@@ -144,7 +136,7 @@ def add_post():
         db.session.commit()
         flash("Your post is now live!", "success")
         return redirect(url_for("main.index"))
-    return render_template("add_post.html", form=form)
+    return render_template("add_post.html", form=form, legend="Add Post")
 
 
 @main.route("/post/<int:post_id>/update_post", methods=["GET", "POST"])
